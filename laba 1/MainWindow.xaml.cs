600f04566bb02ae0930bbb8491bc6f27943b8d5e
abdotar <abdotar1@mail.ru>
2016-11-23 08:58:43 +0300

    задание 1

--- laba 1/MainWindow.xaml.cs
+++ laba 1/MainWindow.xaml.cs
@@ -93,11 +93,11 @@ namespace laba_1
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            //Для массива из n чисел найти номер первого элемента, большего 25.
+            /* Сколько элементов массива больше своих «соседей»,­ т.е. предыдущег­о и последующе­го.
+             Первый и последний элементы не рассматрив­ать.*/
 
             ArrayList myAL = new ArrayList();
-            int bigers = -1; //количество элементов больше своих соседей 
-
+            int bigers = 0; //количество элементов больше своих соседей 
             int index;
             int itemcount = Convert.ToInt32(elements_tb.Text);
             Random rn = new Random();
@@ -111,21 +111,17 @@ namespace laba_1
                 mass_lb.Items.Add(number);
             }
 
-            for (int i = 0; i < itemcount; i++)
+            for (int i = 1; i < itemcount - 1; i++)
             {
                 int somenumber = Convert.ToInt32(myAL[i]);//сравниваемое число
 
-                if (somenumber > 25)
+                if (somenumber > Convert.ToInt32(myAL[i + 1]) && somenumber > Convert.ToInt32(myAL[i - 1]))
                 {
-                    bigers = i;
-                    mass_lb.Items.Add("номер первого элемента, большего 25");
-                    mass_lb.Items.Add(bigers);
-                    break;
+                    bigers++;
                 }
-
             }
-            if (bigers == -1)
-                mass_lb.Items.Add("элементов > 25 в массиве нет");
+            mass_lb.Items.Add("Количество элементов массива больше своих «соседей» ");
+            mass_lb.Items.Add(bigers);
         }
 
       /*  private void Window_KeyUp(object sender, KeyEventArgs e)
